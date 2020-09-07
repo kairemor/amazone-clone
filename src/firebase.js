bfb3fa54f70ae6778116c5fdad53f040da1e5774
kmtc44 <kairmor@yahoo.com>
2020-09-07 13:56:54 +0000

    setup project clean not used file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4yNAxt-QCVR5D8RYW1nIE1TMFmy057qQ",
+  authDomain: "fir-26279.firebaseapp.com",
+  databaseURL: "https://fir-26279.firebaseio.com",
+  projectId: "fir-26279",
+  storageBucket: "fir-26279.appspot.com",
+  messagingSenderId: "1056690043434",
+  appId: "1:1056690043434:web:d12155372c9489100fa153",
+  measurementId: "G-5Z94XYTBM0"
+};
